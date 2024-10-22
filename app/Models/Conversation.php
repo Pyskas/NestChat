f493a2a5fc380f32251bdbdbf5de97d6d7f815cb
Ivan Podoprigorov <ivan.podoprigorov@mail.ru>
2024-10-22 22:15:07 +0500

    Добавил модели,заполнил миграции и фактории

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'user_id1',
+        'user_id2',
+        'last_message_id',
+    ];
+
+    public function lastMessage()
+    {
+        return $this->belongsToMany(Message::class, 'last_message_id');
+    }
+
+    public function user1()
+    {
+        return $this->belongsToMany(User::class, 'user_id1');
+    }
+
+    public function user2()
+    {
+        return $this->belongsToMany(User::class, 'user_id2');
+    }
 }
